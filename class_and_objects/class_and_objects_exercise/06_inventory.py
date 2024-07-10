cb7bbcca44255622c933289e2bdeb422ca300da7
IvaVasileva08 <vasileva_iva_i@abv.bg>
2024-07-10 20:57:17 +0300

    Add files via upload

---
+++ class_and_objects/class_and_objects_exercise/06_inventory.py
@@ -0,0 +1,13 @@
+class Inventory:
+    def __init__(self, __capacity):
+        self.__capacity = __capacity
+        self.items = []
+    def add_item(self, item: str):
+        if self.__capacity > len(self.items):
+            self.items.append(item)
+        else:
+            return "not enough room in the inventory"
+    def get_capacity(self):
+        return self.__capacity
+    def __repr__(self):
+        return f"Items: {', '.join(self.items)}.\nCapacity left: {self.__capacity - len(self.items)}"
